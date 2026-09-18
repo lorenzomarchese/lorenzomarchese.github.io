a22131b0eaa9c10bc79d7733aa1420cb22e508cd
Lorenzo <lorenzo.marchese4@gmail.com>
2026-09-19 01:02:34 +0200

    Layout refactor: drop hover.css, cleaner board markup, jQuery 3.7, ignore revamp plan

--- js/script.js
+++ js/script.js
@@ -6,54 +6,51 @@ $(document).ready(function(){
   for (var i = 1; i <= 100; i++) {
     $('.grid').append("<div class='cell'/>");
   };
+  counter = 0;
 });
+
 //Rimuove l'animazione e prepara la griglia per la partita cliccando su "NEW GAME"
 counter = 0;
 $(document).ready(function(){
-  $('#newgame').click(function(){
-    $('.grid').removeClass('wave');
-    $('.cell').css({"background-color":"rgba(255,255,255,0.7)"}).css({"margin-top":"2px"}).empty();
+  $('#newgame').on('click', function(){
+    $('.cell').removeClass('clicked green').empty();
     $('.cell').removeClass('clicked green');
     $(".cell").each(function(n, div) {
-    div.id = "div" + (n); //n starts at 1
-    if(n<10){
-      div.id = "div" +"0"+(n);
-    }
-  });
-  counter = 0;
+      div.id = "div" + (n); //n starts at 1
+      if(n<10){
+        div.id = "div" +"0"+(n);
+      }
+    });
+    counter = 0;
   });
 });
 
 var cellId, cellClass;
-var cellColor = $('.cell').css('background-color');
-$(document).ready(function(){
-  $('.cell').click(function(){
-    cellId = $(this).attr('id');
-    cellClass = $(this).attr('class');
-    if(cellClass.search("clicked") == -1){
-      var x = getX(cellId);
-      var y = getY(cellId);
-      //if ($('#'+cellId).css('background-color') == "rgb(118, 233, 153)" || counter == 0) {
-      if ($('#'+cellId).hasClass("green") || counter == 0) {
-        allowed(x,y);
-        counter++;
-        $('#'+cellId).html(''+counter).removeAttr('id').addClass('clicked');
-      }else{
+$(document).on('click', '.cell', function(){
+  cellId = $(this).attr('id');
+  cellClass = $(this).attr('class');
+  if(cellClass.search("clicked") == -1){
+    var x = getX(cellId);
+    var y = getY(cellId);
+    //if ($('#'+cellId).css('background-color') == "rgb(118, 233, 153)" || counter == 0) {
+    if ($('#'+cellId).hasClass("green") || counter == 0) {
+      allowed(x,y);
+      counter++;
+      $('#'+cellId).html(''+counter).removeAttr('id').addClass('clicked');
+    }else{
 
-      }
-      endGame();
     }
-  });
+    endGame();
+  }
 });
 
 $(document).ready(function(){
-  $('#endnewgame').click(function(){
+  $('#endnewgame').on('click', function(){
     $('#endgame').fadeOut('fast');
-    $('.cell').fadeIn('slow');
     $('#newgame').click();
   });
 
-  $('#examplegif').click(function(){
+  $('#examplegif').on('click', function(){
     $('#gif').slideToggle(500);
   });
 });
@@ -78,37 +75,25 @@ function getX(s){
   }
 }
 function allowed(x,y){
-  $('.cell').css({"background-color":"rgba(255,255,255,0.7)"}).removeClass('green');
-  $('#div'+(y+3)+""+x).css({"background-color":"rgb(118,233,153)"}).addClass('green');
-  $('#div'+(y-3)+""+x).css({"background-color":"rgb(118,233,153)"}).addClass('green');
-  $('#div'+y+""+(x+3)).css({"background-color":"rgb(118,233,153)"}).addClass('green');
-  $('#div'+y+""+(x-3)).css({"background-color":"rgb(118,233,153)"}).addClass('green');
-  $('#div'+(y+2)+""+(x+2)).css({"background-color":"rgb(118,233,153)"}).addClass('green');
-  $('#div'+(y+2)+""+(x-2)).css({"background-color":"rgb(118,233,153)"}).addClass('green');
-  $('#div'+(y-2)+""+(x+2)).css({"background-color":"rgb(118,233,153)"}).addClass('green');
-  $('#div'+(y-2)+""+(x-2)).css({"background-color":"rgb(118,233,153)"}).addClass('green');
-
-  // $('.cell').css({"background-color":"rgba(255,255,255,0.7)"}).removeClass('green green-anim');
-  // $('#div'+(y+3)+""+x).addClass('green');
-  // $('#div'+(y-3)+""+x).addClass('green');
-  // $('#div'+y+""+(x+3)).addClass('green');
-  // $('#div'+y+""+(x-3)).addClass('green');
-  // $('#div'+(y+2)+""+(x+2)).addClass('green');
-  // $('#div'+(y+2)+""+(x-2)).addClass('green');
-  // $('#div'+(y-2)+""+(x+2)).addClass('green');
-  // $('#div'+(y-2)+""+(x-2)).addClass('green');
-
+  $('.cell').removeClass('green');
+  $('#div'+(y+3)+""+x).addClass('green');
+  $('#div'+(y-3)+""+x).addClass('green');
+  $('#div'+y+""+(x+3)).addClass('green');
+  $('#div'+y+""+(x-3)).addClass('green');
+  $('#div'+(y+2)+""+(x+2)).addClass('green');
+  $('#div'+(y+2)+""+(x-2)).addClass('green');
+  $('#div'+(y-2)+""+(x+2)).addClass('green');
+  $('#div'+(y-2)+""+(x-2)).addClass('green');
 }
 
 function endGame(){
   if(counter == 100){
-      $('.cell').fadeOut('slow');
+      $('.cell').removeClass('green');
       $('.endtext').empty().append('YOU<br>WON!!');
       $('#endgame').fadeIn('slow');
-      $('#endscore').hide();
+      $('.endscore').hide();
   }
   if($('.cell').hasClass('green') == false && counter < 100){
-    $('.cell').fadeOut('slow');
     $('.endtext').empty().append('GAME<br>OVER!!');
     $('.endscore').show().empty().append('SCORE: '+counter);
     $('#endgame').fadeIn('slow');
@@ -117,7 +102,7 @@ function endGame(){
 }
 
 $(document).ready(function(){
-  $('a[href*=#]').click(function() {
+  $('a[href*="#"]').on('click', function() {
   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
   && location.hostname == this.hostname) {
   var $target = $(this.hash);
@@ -129,4 +114,3 @@ $(document).ready(function(){
   }
  });
 });
-
